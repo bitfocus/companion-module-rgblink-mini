8ca1e181d33a4157fd7b37782df76bd6f847d8ed
Marcin Gumułka <marcingum@gmail.com>
2022-07-24 10:35:21 +0200

    add single action with two settings: mode and signal source

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 /*
 TODO
-* sprawdzanie połaczneia na początku - OK pokazuje się, nawet gdy urządzenie niepodłączone do prądu
 * inne akcje (np pip)
 * przetestować feedbacki do pip
 * presets
@@ -143,7 +142,42 @@ class instance extends instance_skel {
 				//console.log('onAction');
 				this.sendCommand(SWITCH_MODE_MSG[action.options.mode]);
 			},
-		}		
+		}
+		actions['switch_mode_and_source'] = {
+			label: 'Select source and target',
+			options: [
+				{
+					type: 'dropdown',
+					label: 'Source number',
+					id: 'sourceNumber',
+					default: '1',
+					tooltip: 'Choose source number, which should be selected',
+					choices: [
+						{ id: '1', label: '1' },
+						{ id: '2', label: '2' },
+						{ id: '3', label: '3' },
+						{ id: '4', label: '4' }
+					],
+					minChoicesForSearch: 0
+				},
+				{
+					type: 'dropdown',
+					label: 'Mode',
+					id: 'mode',
+					default: SWITCH_MODE_AUTO,
+					tooltip: 'Choose mode',
+					choices: [
+						{ id: SWITCH_MODE_AUTO, label: 'Auto (Take)' },
+						{ id: SWITCH_MODE_TBAR, label: 'T-BAR (Preview)' },
+					],
+					minChoicesForSearch: 0
+				},
+			],
+			callback: (action, bank) => {
+				//console.log('onAction');
+				this.sendCommand(SWITCH_MODE_MSG[action.options.mode] + SWITCH_TO_SOURCE_MSG[action.options.sourceNumber]);
+			},
+		}
 
 		this.setActions(actions)
 	}
